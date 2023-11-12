cb5b515aa06b8fd914f201904e080e4bd4633953
Peter Ma <peterma1999@gmail.com>
2023-11-11 19:23:52 -0500

    Task 2: Fix addition of year

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 def battery_needs_service(last_service_date, current_date, service_years):
-    if last_service_date + service_years < current_date:
+    if (last_service_date.replace(year=last_service_date.year 
+                                  + service_years) < current_date):
         return True
     else:
         return False
